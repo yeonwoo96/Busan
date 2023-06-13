2fb7a3e9506b1f98bf4425961fd00723b21f93e3
yeonwoo shin <tlsxkrqhd@naver.com>
2023-06-13 22:58:58 +0900

    second

--- src/Sections/Info.tsx
+++ src/Sections/Info.tsx
@@ -4,16 +4,40 @@ import styled from "styled-components";
 const InfoContainer = styled.div`
   position: relative;
   margin-top: 100vh;
+  width: 100vw;
   height: 100vh;
   z-index: 6;
+  display: flex;
+  flex-flow: column;
 `;
 const Title = styled.h2`
+  margin-top: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   color: transparent;
+  span {
+    position: relative;
+    text-indent: -99999px;
+    background: url("src/assets/images/main_sub_title_01.svg") no-repeat center
+      center / contain;
+    width: 80px;
+    height: 2.4rem;
+    color: transparent;
+    display: block;
+    margin: 0 auto;
+    @media (max-width: 1024px) {
+      height: 1.4rem;
+    }
+  }
 `;
 const Info = () => {
   return (
     <InfoContainer>
-      <Title>안내</Title>
+      <Title>
+        <span>안내</span>
+      </Title>
     </InfoContainer>
   );
 };
